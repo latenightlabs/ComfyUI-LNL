cba4e7939035cc5d9983d14c78a2289ad5eaae98
ericlatenight <157748925+ericlatenight@users.noreply.github.com>
2024-03-27 19:57:01 -0700

    Delete ComfyUI-LNL directory

--- ComfyUI-LNL/__init__.py
+++
@@ -1,5 +0,0 @@
-from .modules.nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
-from .modules import server
-
-WEB_DIRECTORY = "./web"
-__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS", "WEB_DIRECTORY"]
