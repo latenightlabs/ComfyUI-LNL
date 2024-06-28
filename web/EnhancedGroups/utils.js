9ef95edfe522d04c823d2bb29dd1633a91db281b
Miran Brajsa <miran.brajsa@gmail.com>
2024-06-28 20:28:19 +0200

    Added groups and links.

--- web/EnhancedGroups/utils.js
+++ web/EnhancedGroups/utils.js
@@ -1,5 +1,6 @@
 'use strict';
 
+import { app } from "../../../scripts/app.js"; // For LiteGraph
 import { api } from "../../../scripts/api.js";
 
 // API utils
@@ -25,41 +26,106 @@ export async function fetchGroupData(groupId) {
 
 // Litegraph utils
 export function addGroupToGraph(app, groupData) {
-    // this.last_node_id = 0;
-    // this.last_link_id = 0;
+    // Remap node IDs
+    const nodes = groupData.nodes;
+    const nodeIDMapping = {};
+    if (nodes) {
+        for (let i = 0; i < nodes.length; ++i) {
+            const node = nodes[i];
+            nodeIDMapping[node.id] = app.graph.last_node_id + i + 1;
+            node.id = nodeIDMapping[node.id];
+        }
+    }
+
+    // Remap link IDs and link's origin/target node IDs
+    const links = groupData.links;
+    const linkIDMapping = {};
+    if (links && links.constructor === Array) {
+        for (let i = 0; i < links.length; ++i) {
+            const link = links[i];
+            linkIDMapping[link[0]] = app.graph.last_link_id + i + 1;
+            link[0] = linkIDMapping[link[0]];
+            link[1] = nodeIDMapping[link[1]];
+            link[3] = nodeIDMapping[link[3]];
+        }
+    }
+    app.graph.last_link_id += links.length;
+
+    // Remap nodes' input/output links
+    if (nodes) {
+        for (let i = 0; i < nodes.length; ++i) {
+            const node = nodes[i];
+
+            const linkArrays = [];
+            if (node.inputs) {
+                linkArrays.push(node.inputs);
+            }
+            if (node.outputs) {
+                linkArrays.push(node.outputs);
+            }
+            for (let j = 0; j < linkArrays.length; ++j) {
+                const inOutItem = linkArrays[j];
+                for (let k = 0; k < inOutItem.length; ++k) {
+                    const connection = inOutItem[k];
+                    if (connection.links) {
+                        for (let l = 0; l < connection.links.length; ++l) {
+                            connection.links[l] = linkIDMapping[connection.links[l]];
+                        }
+                    }
+                    if (connection.link) {
+                        connection.link = linkIDMapping[connection.link];
+                    }
+                }
+            }
+        }
+    }
 
     // Add links
-    // Add nodes
-    const nodes = groupData.nodes;
-    const nodeIdMapping = {};
-    for (let i = 0; i < nodes.length; ++i) {
-        const node = nodes[i];
-        nodeIdMapping[node.id] = app.graph.last_node_id + i + 1;
-        node.id = nodeIdMapping[node.id];
+    if (links && links.constructor === Array) {
+        const preparedLinks = [];
+        for (let i = 0; i < links.length; ++i) {
+            const link_data = links[i];
+
+            var link = new LiteGraph.LLink();
+            link.configure(link_data);
+            preparedLinks.push(link);
+        }
+        if (!app.graph.links) {
+            app.graph.links = [];
+        }
+        app.graph.links.push(...preparedLinks);
     }
+
+    // Add nodes
     if (nodes) {
+        // Create new nodes
         for (let i = 0; i < nodes.length; ++i) {
             const n_info = nodes[i];
             const node = LiteGraph.createNode(n_info.type, n_info.title);
 
-            console.log(`new node id: ${n_info.id}`);
             node.id = n_info.id;
             app.graph.add(node, true);
         }
 
-        //configure nodes afterwards so they can reach each other
+        // Configure them with saved date
         for (let i = 0; i < nodes.length; ++i) {
             const n_info = nodes[i];
             const node = app.graph.getNodeById(n_info.id);
-            console.log(`node id A: ${node.id}`)
             if (node) {
                 node.configure(n_info);
             }
-            console.log(`node id B: ${node.id}`)
         }
     }
     
     // Add groups
+    const groups = groupData.groups;
+    if (groups) {
+        for (let i = 0; i < groups.length; ++i) {
+            var group = new LiteGraph.LGraphGroup();
+            group.configure(groups[i]);
+            app.graph.add(group);
+        }
+    }
 
     // Update the canvas
     app.graph.updateExecutionOrder();
