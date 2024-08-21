31bdb4ba4ae099a159f38a6d8d26eb86426b6cc6
Miran Brajsa <miran.brajsa@gmail.com>
2024-08-21 16:57:51 +0200

    Removed all deprecated versions.

--- modules/nodes.py
+++ modules/nodes.py
@@ -32,11 +32,8 @@ def getImageBatch(full_video_path, frames_to_process, select_every_nth_frame, st
 
     return (imageBatch, target_frame_time)
 
-class FrameSelector:
+class FrameSelectorV3():
 
-    supported_video_extensions =  ['webm', 'mp4', 'mkv']
-
-    @classmethod
     def INPUT_TYPES(s):
         input_dir = folder_paths.get_input_directory()
         files = []
@@ -54,80 +51,28 @@ class FrameSelector:
                 "unique_id": "UNIQUE_ID"
             },
         }
-
-    RETURN_TYPES = ("IMAGE", "IMAGE", "INT", "INT", "INT", "INT", "INT", "VHS_AUDIO",)
-    RETURN_NAMES = ("Current image", "Image Batch (in/out)", "Frame count (rel)", "Frame count (abs)", "Current frame (rel)", "Current frame (abs)", "Frame rate", "audio",)
-    OUTPUT_NODE = True
-    CATEGORY = "LNL"
-    FUNCTION = "process_video"
-
-    def process_video(
-        self,
-        video_path,
-        prompt=None,
-        unique_id=None
-    ):
-        prompt_inputs = prompt[unique_id]["inputs"]
-        full_video_path = os.path.join(folder_paths.base_path, video_path)
-
-        in_point = prompt_inputs["in_out_point_slider"]["startMarkerFrame"]
-        out_point = prompt_inputs["in_out_point_slider"]["endMarkerFrame"]
-        current_frame = prompt_inputs["in_out_point_slider"]["currentFrame"]
-        total_frames = prompt_inputs["in_out_point_slider"]["totalFrames"]
-        frame_rate = prompt_inputs["in_out_point_slider"]["frameRate"]
-
-        select_every_nth_frame = prompt_inputs["select_every_nth_frame"]
-
-        frames_to_process = out_point - in_point + 1
-        starting_frame = in_point
-
-        (current_image, _) = getImageBatch(full_video_path, 1, 1, current_frame, "Disabled", 0, 0)
-        (in_out_images, target_frame_time) = getImageBatch(full_video_path, frames_to_process, select_every_nth_frame, starting_frame, "Disabled", 0, 0)
-
-        audio = lambda: lnl_get_audio(full_video_path, starting_frame * target_frame_time,
-                               frames_to_process*target_frame_time*select_every_nth_frame)
-
-        return (
-            current_image,
-            in_out_images,
-            frames_to_process,
-            total_frames,
-            current_frame - in_point + 1,
-            current_frame,
-            frame_rate,
-            lnl_lazy_eval(audio),
-        )
-
-class FrameSelectorV2(FrameSelector):
-
-    RETURN_TYPES = ("IMAGE", "IMAGE", "INT", "INT", "STRING", "INT", "INT", "INT", "INT", "INT", "VHS_AUDIO",)
-    RETURN_NAMES = ("Current image", "Image Batch (in/out)", "Frame in", "Frame out", "Filename", "Frame count (rel)", "Frame count (abs)", "Current frame (rel)", "Current frame (abs)", "Frame rate", "audio",)
-    OUTPUT_NODE = True
-    CATEGORY = "LNL"
-    FUNCTION = "process_video"
-
-    def process_video(
-        self,
-        video_path,
-        prompt=None,
-        unique_id=None
-    ):
-        prompt_inputs = prompt[unique_id]["inputs"]
-        in_point = prompt_inputs["in_out_point_slider"]["startMarkerFrame"]
-        out_point = prompt_inputs["in_out_point_slider"]["endMarkerFrame"]
-
-        result = super().process_video(video_path, prompt, unique_id)
-        return result[:2] + (in_point, out_point, video_path,) + result[2:]
-
-class FrameSelectorV3(FrameSelectorV2):
-
+    
     @classmethod
     def INPUT_TYPES(s):
-        v2_input_types = FrameSelectorV2.INPUT_TYPES()
-        v2_input_types["required"]["force_size"] = (["Disabled", "Custom Height", "Custom Width", "Custom", "256x?", "?x256", "256x256", "512x?", "?x512", "512x512"],)
-        v2_input_types["required"]["custom_width"] = ("INT", {"default": 512, "min": 0, "max": 8192, "step": 8})
-        v2_input_types["required"]["custom_height"] = ("INT", {"default": 512, "min": 0, "max": 8192, "step": 8})
-        return v2_input_types
+        input_dir = folder_paths.get_input_directory()
+        files = []
+        for f in os.listdir(input_dir):
+            if os.path.isfile(os.path.join(input_dir, f)):
+                file_parts = f.split('.')
+                if len(file_parts) > 1 and (file_parts[-1] in FrameSelector.supported_video_extensions):
+                    files.append(f"input/{f}")
+        return {
+            "required": {
+                "video_path": (sorted(files),),
+                "force_size": (["Disabled", "Custom Height", "Custom Width", "Custom", "256x?", "?x256", "256x256", "512x?", "?x512", "512x512"],),
+                "custom_width": ("INT", {"default": 512, "min": 0, "max": 8192, "step": 8}),
+                "custom_height": ("INT", {"default": 512, "min": 0, "max": 8192, "step": 8}),
+            },
+            "hidden": {
+                "prompt": "PROMPT",
+                "unique_id": "UNIQUE_ID"
+            },
+        }
 
     RETURN_TYPES = ("IMAGE", "IMAGE", "INT", "INT", "STRING", "INT", "INT", "INT", "INT", "INT", "VHS_AUDIO",)
     RETURN_NAMES = ("Current image", "Image Batch (in/out)", "Frame in", "Frame out", "Filename", "Frame count (rel)", "Frame count (abs)", "Current frame (rel)", "Current frame (abs)", "Frame rate", "audio",)
@@ -179,12 +124,8 @@ class FrameSelectorV3(FrameSelectorV2):
         )
 
 NODE_CLASS_MAPPINGS = {
-    "LNL_FrameSelectorV3": FrameSelectorV3,
-    "LNL_FrameSelectorV2": FrameSelectorV2,
-    "LNL_FrameSelector": FrameSelector,
+    "LNL_FrameSelectorV3": FrameSelectorV3
 }
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "LNL_FrameSelectorV3": "LNL Frame Selector V3",
-    "LNL_FrameSelectorV2": "LNL Frame Selector V2 [Deprecated] ⛔️",
-    "LNL_FrameSelector": "LNL Frame Selector [Deprecated] ⛔️",
+    "LNL_FrameSelectorV3": "LNL Frame Selector"
 }
