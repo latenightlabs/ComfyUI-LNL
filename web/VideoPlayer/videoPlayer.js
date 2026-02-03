e8a841539cf6c569748c9fb9b52747b356772ebf
Maxime Mouflard <maxime.mouflard@maxtronics.fr>
2026-02-03 11:16:39 +0100

    Align queued/waiting overlay states

--- web/VideoPlayer/videoPlayer.js
+++ web/VideoPlayer/videoPlayer.js
@@ -1277,21 +1277,11 @@ function setWaitingForOtherPause(activeNode, enabled) {
         }
         if (enabled) {
             node._lnlWaitingForOtherPause = true;
-            node._lnlQueuedBeforeWait = !!node._lnlQueuedActive;
-            node._lnlProcessingBeforeWait = !!node._lnlProcessingActive;
-            node._lnlQueuedActive = false;
             node.previewWidget.setProcessing(true, "Waiting for other pause...");
         } else if (node._lnlWaitingForOtherPause) {
             node._lnlWaitingForOtherPause = false;
-            const restoreQueued = !!node._lnlQueuedBeforeWait;
-            const restoreProcessing = !!node._lnlProcessingBeforeWait && !restoreQueued;
-            node._lnlQueuedBeforeWait = false;
-            node._lnlProcessingBeforeWait = false;
-            if (restoreQueued) {
-                node._lnlQueuedActive = true;
+            if (node._lnlQueuedActive) {
                 node.previewWidget.setProcessing(true, "Queued for execution...");
-            } else if (restoreProcessing) {
-                node.previewWidget.setProcessing(true, "Processing media...");
             } else {
                 node.previewWidget.setProcessing(false);
             }
